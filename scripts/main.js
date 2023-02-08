54e5f304df603e508838f427bc78c918b49ff344
s3kken <your.unicorn0@gmail.com>
2023-02-08 22:45:38 +0700

    добавление карточки в первый столбик

--- scripts/main.js
+++ scripts/main.js
@@ -1,21 +1,22 @@
 let eventBus = new Vue()
 
-Vue.component('KanbanList',{
+Vue.component('columns',{
 
 template: `
 <div class="list-notes">
 <div class="row-col">
+<create-card></create-card>
+    <div class="col">
+        <card-form v-for="card in cardsOne" :card="card"></card-form>
+    </div>
     <div class="col">
         
     </div>
     <div class="col">
-
+        
     </div>
     <div class="col">
-
-    </div>
-    <div class="col">
-
+        
     </div>
 </div>
 </div>
@@ -29,27 +30,31 @@ data() {
     }
 },
 mounted() {
-    eventBus.$on('card-submitted', createCard => {
-        this.cardsOne.push(createCard)
+    eventBus.$on('card-submitted', card => {
+        this.cardsOne.push(card)
     })
 },
 
 })
 
-Vue.component('card',{
+Vue.component('card-form',{
     template: `
     <div class="cardOne">
-    <p>{{ createCard.title }}</p>
-    <p>{{ createCard.task }}</p>
-    <p>{{ createCard.deadline }}</p>
+    <p>{{ card.title }}</p>
+    <p>{{ card.task }}</p>
+    <p>Deadline:{{ card.deadline }}</p>
+    <p>Дата создания:{{card.dateCreate}}</p>
     </div>
-    `
+    `,
+    props: {
+        card: Object
+    }
 })
 
-Vue.component('createCard',{
+Vue.component('create-card',{
     template:`
     <div class="forms-create-card">
-        <form class="text-form-card">
+        <form class="text-form-card" @submit.prevent="onSubmit">
             <label for="title">Заголовок</label>
             <input v-model="title" id="title" type="text" placeholder="title">
             <textarea v-model="task"></textarea>
@@ -75,21 +80,21 @@ Vue.component('createCard',{
     methods:{
         onSubmit(){
             if(this.title && this.task && this.deadline){
-                let createCard = {
+                let card = {
                     title: this.title,
                     task: this.task,
                     deadline: this.deadline,
-                    dateCreate: null
+                    dateCreate: new Date().toLocaleString()
                 }
-                this.$emit('card-submitted', createCard)
+                eventBus.$emit('card-submitted', card)
 
                 this.title = null
                 this.task = null
                 this.deadline = null
             } else {
-                if (!this.title) this.errors.push("Заполните первый пункт!")
-                if (!this.task) this.errors.push("Заполните второй пункт!")
-                if (!this.deadline) this.errors.push("Заполните третий пункт!")
+                if (!this.title) this.errors.push("Заполните заголовок!")
+                if (!this.task) this.errors.push("Заполните описание задачи!")
+                if (!this.deadline) this.errors.push("Выберите дедлайн!")
             }
         }
     }
