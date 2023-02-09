938be02b4f416ddaac1c90a2094c4f5a95702b56
s3kken <your.unicorn0@gmail.com>
2023-02-09 20:52:59 +0700

    изменила структуру и сделала редактирование(работает!!)

--- scripts/main.js
+++ scripts/main.js
@@ -4,21 +4,13 @@ Vue.component('columns',{
 
 template: `
 <div class="list-notes">
-<div class="row-col">
-<create-card></create-card>
-    <div class="col">
-        <card-form v-for="card in cardsOne" :card="card"></card-form>
+    <div class="row-col">
+    <create-card></create-card>
+        <column-planned-tasks :cardList="cardsOne"></column-planned-tasks>
+        <column-tasks-work :cardList="cardsTwo"></column-tasks-work>
+        <column-testing :cardList="cardsThree"></column-testing>
+        <column-completed-tasks :cardList="cardsFour"></column-completed-tasks>
     </div>
-    <div class="col">
-        
-    </div>
-    <div class="col">
-        
-    </div>
-    <div class="col">
-        
-    </div>
-</div>
 </div>
 `,
 data() {
@@ -37,33 +29,133 @@ mounted() {
 
 })
 
+Vue.component('column-planned-tasks', {
+    props: {
+        cardList: [],
+    },
+    template: `
+    <div class="col">
+        <card-form
+            v-for="card in cardList"
+            :card="card">
+        </card-form>
+    </div>
+    `,
+    methods: {
+
+    }
+})
+
+Vue.component('column-tasks-work', {
+    props: {
+        cardList: [],
+    },
+    template: `
+    <div class="col">
+        <card-form
+            v-for="card in cardList"
+            :card="card">
+        </card-form>
+    </div>
+    `,
+    methods: {
+
+    }
+})
+
+Vue.component('column-testing', {
+    props: {
+        cardList: [],
+    },
+    template: `
+    <div class="col">
+        <card-form
+            v-for="card in cardList"
+            :card="card">
+        </card-form>
+    </div>
+    `,
+    methods: {
+        
+    }
+})
+
+Vue.component('column-completed-tasks', {
+    props: {
+        cardList: [],
+    },
+    template: `
+    <div class="col">
+        <card-form
+            v-for="card in cardList"
+            :card="card">
+        </card-form>
+    </div>
+    `,
+})
+
 Vue.component('card-edit',{
     template:`
     <div class="cardOne">
-        <form class="text-form-card">
-            <label for="title">Заголовок</label>
-            <input v-model="title" id="title" type="text" placeholder="title">
-            <textarea v-model="task" placeholder="task description"></textarea>
-            <input v-model="deadline" type="date">
-            <button type="submit">Yes</button>
-            <button type="submit">No</button>
+    <button type="submit" v-if="show === false" @click="$emit('Edit', isEdit())">Редактирование</button>
+        <form class="text-form-card" v-if="show === true">
+            <label for="title">Редактирование</label>
+            <input v-model="title" id="title" type="text" :placeholder="card.title">
+            <textarea v-model="task" :placeholder="card.task"></textarea>
+            <p>Дэдлайн: {{ card.deadline }}</p>
+            <p>Дата создания: {{ card.dateCreate }}</p>
+            <button type="submit" @click="$emit('Edit', isEdit())">Yes</button>
+            <button type="submit" @click="$emit('Edit', show = false)">No</button>
         </form>
     </div>
     `,
+    data() {
+        return {
+            show: false,
+            title: this.card.title,
+            task: this.card.task,
+        }
+    },
+    props: {
+        card: Object,
+    },
+    methods: {
+        isEdit() {
+            if (this.show == false)
+                this.show = true;
+            else {
+                if (this.title)
+                    this.card.title = this.title;
+
+                if (this.task)
+                    this.card.task = this.task;
+
+                this.show = false;
+            }
+
+            return this.show;
+        }
+    },
 })
 
 Vue.component('card-form',{
     template: `
     <div class="cardOne">
+    <div v-if="edit === false">
     <p>{{ card.title }}</p>
     <p>{{ card.task }}</p>
     <p>Deadline:{{ card.deadline }}</p>
     <p>Дата создания:{{card.dateCreate}}</p>
-
+    <button type="submit">
+    Переместить
+    </button>
+    </div>
+    <card-edit :card="card" @Edit="edit = $event"></card-edit>
     </div>
     `,
     props: {
-        card: Object
+        card: Object,
+        edit: Boolean,
     }
 })
 
@@ -100,7 +192,9 @@ Vue.component('create-card',{
                     title: this.title,
                     task: this.task,
                     deadline: this.deadline,
-                    dateCreate: new Date().toLocaleString()
+                    dateCreate: new Date().toLocaleString(),
+                    reason: null,
+                    completed: null,
                 }
                 eventBus.$emit('card-submitted', card)
 
@@ -120,7 +214,7 @@ let app = new Vue({
     el: '#app',
     data(){
         return{
-            check: false
+
         }
     }
 
